6ae1aa30a166b0e7316562cac12dd86e6f3e4e4c
wesolypl <wesolowski.patryk@zhr.pl>
2019-04-12 17:14:44 +0200

    another sw v.149

--- src/sw.js
+++ src/sw.js
@@ -64,10 +64,10 @@ workbox.routing.registerRoute(
 );
 // workbox.precaching.precacheAndRoute(self.__precacheManifest);
 workbox.precaching.precacheAndRoute([
-  "/static/*.js",
-  "/static/*.map",
+  "/static/\\*.js\\",
+  "/static/\\*.map\\",
   "/index.html",
-  "/*.js",
-  "/*.json",
-  "/*.ico"
+  "/\\*.js\\",
+  "/\\*.json\\",
+  "/\\*.ico\\"
 ]);
