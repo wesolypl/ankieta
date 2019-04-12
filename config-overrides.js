e592f190242a531ae338f33cd417bc9ecd4f9f2e
wesolypl <wesolowski.patryk@zhr.pl>
2019-04-12 14:06:06 +0200

    another sw v.126

--- config-overrides.js
+++ config-overrides.js
@@ -9,7 +9,7 @@ module.exports = function override(config, env) {
     console.log("Production build - Adding Workbox for PWAs");
     // Extend the default injection config with required swSrc
     const workboxConfig = {
-      ...defaultInjectConfig,
+      exclude: [/\.map$/, /^(?:asset-)manifest.*\.js(?:on)?$/],
       swSrc: path.join(__dirname, "src", "sw.js"),
       swDest: path.join(__dirname, "build", "service-worker.js"),
       importWorkboxFrom: "local"
