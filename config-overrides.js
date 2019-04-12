cfe8298039ec09cb884e9e77aa1a6aeca06741bf
wesolypl <wesolowski.patryk@zhr.pl>
2019-04-12 13:19:09 +0200

    another sw v.123

--- config-overrides.js
+++ config-overrides.js
@@ -10,7 +10,9 @@ module.exports = function override(config, env) {
     // Extend the default injection config with required swSrc
     const workboxConfig = {
       ...defaultInjectConfig,
-      swSrc: path.join(__dirname, "src", "sw.js")
+      swSrc: path.join(__dirname, "src", "sw.js"),
+      swDest: path.join(__dirname, "build", "service-worker.js"),
+      importWorkboxFrom: "local"
     };
     config = rewireWorkboxInject(workboxConfig)(config, env);
   }
