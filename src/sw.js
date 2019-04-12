5c803159cd0baa53bfcba024e1bd7e4d99d41109
wesolypl <wesolowski.patryk@zhr.pl>
2019-04-12 11:54:49 +0200

    configure sw for updates and fetch

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,45 @@
+const cacheName = "v0.0.1";
+var urlsToCache = ["/", "/static/css/*.css", "/static/js/*.js", "/index.html"];
+
 self.addEventListener("install", event => {
-  const asyncInstall = new Promise(resolve => {
-    console.log("Waiting to resolve...");
-    setTimeout(resolve, 5000);
-  });
-  event.waitUntil(asyncInstall);
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 self.addEventListener("activate", event => {
-  console.log("activate");
+  const cacheWhitelist = ["v0.0.1"];
+  event.waitUntil(
+    caches.keys().then(cacheName => {
+      return Promise.all(
+        cacheName.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200) {
+          return response;
+        }
+        const responseToCache = response.clone();
+
+        caches.open(cacheName).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+      });
+    })
+  );
 });
