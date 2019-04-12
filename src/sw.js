c0bc9184a9150c9f29dd32280f4b124bb3ae5f1a
wesolypl <wesolowski.patryk@zhr.pl>
2019-04-12 11:23:37 +0200

    make test function install and activate sw

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,6 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js"
-);
-console.log("Hello World");
-workbox.precaching.prechacheAndRoute(self.__precacheManifest || []);
+self.addEventListener("install", event => {
+  console.log("install");
+});
+self.addEventListener("activate", event => {
+  console.log("activate");
+});
