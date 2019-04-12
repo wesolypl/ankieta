2efd78aa405584f29f92150dc8aea148adbafd18
wesolypl <wesolowski.patryk@zhr.pl>
2019-04-12 13:24:49 +0200

    another sw v.125

--- src/sw.js
+++ src/sw.js
@@ -3,14 +3,17 @@ console.log("połączony");
 if (workbox) {
   console.log("workbox załadowany");
 } else {
-  console.log("niezaładowany");
+  console.log("workbox niezaładowany");
 }
-workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
-self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
-self.addEventListener("activate", event =>
-  event.waitUntil(self.clients.claim())
-);
+self.addEventListener("install", event => {
+  console.log("instalacja sw");
+  event.waitUntil(self.skipWaiting());
+});
+self.addEventListener("activate", event => {
+  console.log("aktywacja sw");
+  event.waitUntil(self.clients.claim());
+});
 
 // We need this in Webpack plugin (refer to swSrc option): https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#full_injectmanifest_config
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
