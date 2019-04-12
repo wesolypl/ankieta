584b79c8f0453da0ea243f18e11edfd9039ce25a
wesolypl <wesolowski.patryk@zhr.pl>
2019-04-12 12:06:57 +0200

    another one sw, try 2 or 3

--- src/sw.js
+++ src/sw.js
@@ -1,45 +1,7 @@
-const cacheName = "v0.0.1";
-var urlsToCache = ["/", "/static/css/*.css", "/static/js/*.js", "/index.html"];
+workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-self.addEventListener("activate", event => {
-  const cacheWhitelist = ["v0.0.1"];
-  event.waitUntil(
-    caches.keys().then(cacheName => {
-      return Promise.all(
-        cacheName.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).then(response => {
-        if (!response || response.status !== 200) {
-          return response;
-        }
-        const responseToCache = response.clone();
-
-        caches.open(cacheName).then(cache => {
-          cache.put(event.request, responseToCache);
-        });
-      });
-    })
-  );
-});
+workbox.routing.registerRoute('/', workbox.strategies.networkFirst())
